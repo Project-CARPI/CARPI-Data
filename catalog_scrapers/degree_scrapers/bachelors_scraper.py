f3b4877cc7423fdf1c760d14d9ecd28e8e2f9e78
chickenwang6 <nwang842@gmail.com>
2024-12-03 16:34:05 -0500

    Fix bug with Engineering Core Curriculum

--- catalog_scrapers/degree_scrapers/bachelors_scraper.py
+++ catalog_scrapers/degree_scrapers/bachelors_scraper.py
@@ -310,11 +310,17 @@ def main():
                                     fall_sem = []
                                     spring_sem = []
                                     arch_classes = []
-                                    fall_classes = all_leftpads[i].findAll('div', attrs={'class':'acalog-core'})[0].find('ul').findAll('li')
-                                    spring_classes = all_leftpads[i].findAll('div', attrs={'class':'acalog-core'})[1].find('ul').findAll('li')
+                                    # fall_classes = all_leftpads[i].findAll('div', attrs={'class':'acalog-core'})[0].find('ul').findAll('li')
+                                    # spring_classes = all_leftpads[i].findAll('div', attrs={'class':'acalog-core'})[1].find('ul').findAll('li')
 
                                     fall_val = all_leftpads[i].findAll('div', attrs={'class':'acalog-core'})[0].findAll('ul')
                                     spring_val = all_leftpads[i].findAll('div', attrs={'class':'acalog-core'})[1].findAll('ul')
+
+                                    for a in fall_val:
+                                        fall_classes += a.findAll('li')
+                                    for b in spring_val:
+                                        spring_classes += b.findAll('li')
+
                                     if len(fall_val) > 1:
                                         lol = []
                                         for j in range(0, len(fall_val)):
