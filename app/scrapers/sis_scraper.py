702c28ea39dc44412ddfa217a3c5a35f3e651ece
Raymond Chen <42894676+SameriteRL@users.noreply.github.com>
2025-09-22 11:28:13 -0400

    Resolve missing cookie issue
    Small restructure of functions and an addition of clarifying docstrings.
    
    The issue presented in the last commit turned out to be a result of a gap in my understanding of cookies; a JSESSIONID cookie is assigned on the first call to any SIS page.
    
    I was inadvertently calling get_subjects() before attempting to read a cookie from get_cookie(), at which point the aiohttp session would already have a cookie and therefore would not receive another one from the server.
    
    In addition, I did not know that the aiohttp session automatically stores cookies and sends them in subsequent requests. Therefore, there is no need to explicitly handle cookie logic.

--- app/scrapers/sis_scraper.py
+++ app/scrapers/sis_scraper.py
@@ -5,8 +5,6 @@ import time
 import aiohttp
 import bs4
 
-# from app import logger
-
 
 async def get_subjects(
     session: aiohttp.ClientSession, term: str
@@ -32,57 +30,25 @@ async def get_subjects(
         return data
 
 
-async def get_cookie(session: aiohttp.ClientSession, term: str) -> str:
+async def reset_subject_search(session: aiohttp.ClientSession, term: str) -> None:
     """
-    Fetches a JSESSIONID cookie from the SIS API.
+    Resets the subject search state on the SIS server.
 
-    Must be re-called before each new request to get all courses in a subject.
+    Must be re-called before each new attempt to fetch courses from a different subject.
+    Otherwise, the server will continue returning results from the last subject accessed.
     """
     url = "https://sis9.rpi.edu/StudentRegistrationSsb/ssb/term/search?mode=search"
-    async with session.get(
-        url,
-        params={"term": term},
-        headers={
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Connection": "keep-alive",
-            "Host": "sis9.rpi.edu",
-            "Sec-Ch-Ua": '"Chromium";v="140", "Not=A?Brand";v="24", "Microsoft Edge";v="140"',
-            "Sec-Ch-Ua-Mobile": "?0",
-            "Sec-Ch-Ua-Platform": '"macOS"',
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "Upgrade-Insecure-Requests": "1",
-        },
-    ) as response:
+    async with session.get(url, params={"term": term}) as response:
         response.raise_for_status()
-        cookies = response.cookies
-        headers = response.headers
-        print(cookies or "No cookies")
-        print(headers)
-        return cookies["JSESSIONID"].value
 
 
 async def get_courses_in_subject(
-    session: aiohttp.ClientSession, jsessionid_cookie: str, term: str, subject: str
+    session: aiohttp.ClientSession, term: str, subject: str
 ) -> list[dict[str, str]]:
     """
     Fetches the list of courses for a given subject and term from the SIS API.
 
-    Returned data format is as follows:
-    [
-        {
-            "courseId": "12345",
-            "subject": "CSE",
-            "catalogNbr": "1010",
-            "titleLong": "Introduction to Computer Science",
-            ...
-        },
-        ...
-    ]
+    reset_subject_search() must be called once before each call to this function.
     """
     url = "https://sis9.rpi.edu/StudentRegistrationSsb/ssb/searchResults/searchResults"
     params = {
@@ -93,24 +59,42 @@ async def get_courses_in_subject(
         "sortColumn": "subjectDescription",
         "sortDirection": "asc",
     }
-    headers = {"Cookie": f"JSESSIONID={jsessionid_cookie}"}
-    async with session.get(url, params=params, headers=headers) as response:
+    async with session.get(url, params=params) as response:
         response.raise_for_status()
         data = await response.json()
         return data
 
 
 async def main():
+    """
+    A JSESSIONID cookie is required before accessing any course data, which can be
+    obtained on the first request to any SIS page. The cookie should automatically be
+    included in subsequent requests made with the same aiohttp session.
+
+    reset_subject_search() must be called before each new attempt to fetch courses from
+    a different subject.
+    """
     async with aiohttp.ClientSession(
-        headers={
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0",
-            "Accept": "*/*",
-        }
+        # headers={
+        #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        #     "Accept-Encoding": "gzip, deflate, br, zstd",
+        #     "Accept-Language": "en-US,en;q=0.9",
+        #     "Connection": "keep-alive",
+        #     "Host": "sis9.rpi.edu",
+        #     "Sec-Ch-Ua": '"Chromium";v="140", "Not=A?Brand";v="24", "Microsoft Edge";v="140"',
+        #     "Sec-Ch-Ua-Mobile": "?0",
+        #     "Sec-Ch-Ua-Platform": '"macOS"',
+        #     "Sec-Fetch-Dest": "document",
+        #     "Sec-Fetch-Mode": "navigate",
+        #     "Sec-Fetch-Site": "none",
+        #     "Sec-Fetch-User": "?1",
+        #     "Upgrade-Insecure-Requests": "1",
+        # }
     ) as session:
         term = "202409"
         subjects = await get_subjects(session, term)
-        cookie = await get_cookie(session, term)
-        data = await get_courses_in_subject(session, cookie, term, subjects[0]["code"])
+        await reset_subject_search(session, term)
+        data = await get_courses_in_subject(session, term, subjects[0]["code"])
         print(json.dumps(data, indent=4))
 
 
@@ -118,4 +102,4 @@ if __name__ == "__main__":
     start_time = time.time()
     asyncio.run(main())
     end_time = time.time()
-    # logger.info(f"Execution time: {end_time - start_time} seconds")
+    print(f"Total time elapsed: {end_time - start_time:.2f} seconds")
