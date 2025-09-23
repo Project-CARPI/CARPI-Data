46c34ff750c107d741bac4b6501ae4402f1023b6
Raymond Chen <42894676+SameriteRL@users.noreply.github.com>
2025-09-23 17:16:21 -0400

    Remove test headers from main function

--- app/scrapers/sis_scraper.py
+++ app/scrapers/sis_scraper.py
@@ -217,23 +217,7 @@ async def main():
     reset_course_search() must be called before each new attempt to fetch sections from
     a different subject.
     """
-    async with aiohttp.ClientSession(
-        # headers={
-        #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        #     "Accept-Encoding": "gzip, deflate, br, zstd",
-        #     "Accept-Language": "en-US,en;q=0.9",
-        #     "Connection": "keep-alive",
-        #     "Host": "sis9.rpi.edu",
-        #     "Sec-Ch-Ua": '"Chromium";v="140", "Not=A?Brand";v="24", "Microsoft Edge";v="140"',
-        #     "Sec-Ch-Ua-Mobile": "?0",
-        #     "Sec-Ch-Ua-Platform": '"macOS"',
-        #     "Sec-Fetch-Dest": "document",
-        #     "Sec-Fetch-Mode": "navigate",
-        #     "Sec-Fetch-Site": "none",
-        #     "Sec-Fetch-User": "?1",
-        #     "Upgrade-Insecure-Requests": "1",
-        # }
-    ) as session:
+    async with aiohttp.ClientSession() as session:
         term = "202409"
         subjects = await get_subjects(session, term)
         await reset_class_search(session, term)
